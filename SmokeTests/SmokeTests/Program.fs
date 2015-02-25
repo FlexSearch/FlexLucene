1f883e7b1a4833dabce7a65a5affe16aaef3e535
Seemant Rajvanshi <seemant.rajvanshi@gmail.com>
2015-02-25 13:42:34 +0000

    SmokeTests refactoring

--- SmokeTests/SmokeTests/Program.fs
+++ SmokeTests/SmokeTests/Program.fs
@@ -18,9 +18,13 @@ let mutable hasErrors = false
 let exceptionWrapper (meth : unit -> unit) = 
     try 
         meth()
+        ()
     with e -> 
         hasErrors <- true
+        printfn "------------------------------------------------"
+        printfn "Test name: %s" (meth.GetType().FullName)
         printfn "%A" e
+        printfn "------------------------------------------------"
 
 let ShouldHaveFlexCodec50() = Assert.True(Codec.AvailableCodecs().contains("FlexCodec50"), "FlexCodec50 not found.")
 let ShouldHaveFlexCodec410() = Assert.True(Codec.AvailableCodecs().contains("FlexCodec410"), "FlexCodec410 not found.")
@@ -31,7 +35,6 @@ let ShouldHaveFlexPerFieldPostingsFormat() =
 let IndexingTest(directory : FlexLucene.Store.Directory) = 
     let analyzer = new StandardAnalyzer()
     let config = new IndexWriterConfig(analyzer)
-    config.SetInfoStream(new FlexLucene.Util.PrintStreamInfoStream(new java.io.PrintStream(@"C:\git\FlexLucene.debug"))) |> ignore
     let iwriter = new IndexWriter(directory, config)
     let doc = new Document()
     let text = "This is the text to be indexed."
@@ -54,54 +57,24 @@ let IndexingTest(directory : FlexLucene.Store.Directory) =
     ireader.Close()
     directory.close()
 
+let GetRandomPath() = 
+    let dir = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, Guid.NewGuid().ToString())
+    (new java.io.File(dir)).toPath()
+
 [<Fact>]
 let CodecsShouldLoadProperly() = 
-    ShouldHaveFlexCodec410()
-    ShouldHaveFlexCodec50()
+    ShouldHaveFlexCodec410 |> exceptionWrapper
+    ShouldHaveFlexCodec50 |> exceptionWrapper
 
 [<Fact>]
 let PostingsFormatShouldLoadProperly() = ShouldHaveFlexPerFieldPostingsFormat()
 
 [<Fact>]
-let SimpleIndexingTest() = 
-    let analyzer = new StandardAnalyzer()
-    let directory = new RAMDirectory()
-    let config = new IndexWriterConfig(analyzer)
-    let iwriter = new IndexWriter(directory, config)
-    let doc = new Document()
-    let text = "This is the text to be indexed."
-    doc.Add(new Field("fieldname", text, TextField.TYPE_STORED))
-    iwriter.AddDocument(doc)
-    iwriter.Close()
-    // Now search the index:
-    let ireader = DirectoryReader.Open(directory)
-    let isearcher = new IndexSearcher(ireader)
-    // Parse a simple query that searches for "text":
-    let parser = new QueryParser("fieldname", analyzer)
-    let query = parser.Parse("text")
-    let topDocs = isearcher.Search(query, null, 1000)
-    let hits : ScoreDoc [] = topDocs.ScoreDocs
-    Assert.Equal<int>(1, hits.Length)
-    // Iterate through the results:
-    for i = 0 to hits.Length - 1 do
-        let hitDoc = isearcher.Doc(hits.[i].Doc)
-        Assert.Equal<string>("This is the text to be indexed.", hitDoc.Get("fieldname"))
-    ireader.Close()
-    directory.Close()
-
-[<Fact>]
-let SimplePhysicalIndexingTest() = 
-    IndexingTest(new RAMDirectory())
-    let dir = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, Guid.NewGuid().ToString())
-    let file = new java.io.File(dir) 
-    IndexingTest(new SimpleFSDirectory(file.toPath(), NativeFSLockFactory.INSTANCE))
-
-[<Fact>]
-let SimpleFlexPhysicalIndexingTest() = 
-    IndexingTest(new RAMDirectory())
-    let directoryPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, Guid.NewGuid().ToString("N"))
-    let file = new java.io.File(directoryPath)
-    IndexingTest(FlexFSDirectory.Open(file.toPath()))
+let IndexingTests() = 
+    (fun _ -> IndexingTest(new RAMDirectory())) |> exceptionWrapper
+    (fun _ -> IndexingTest(new SimpleFSDirectory(GetRandomPath(), NativeFSLockFactory.INSTANCE))) |> exceptionWrapper
+    (fun _ -> IndexingTest(new MMapDirectory(GetRandomPath(), NativeFSLockFactory.GetDefault()))) |> exceptionWrapper
+    (fun _ -> IndexingTest(FSDirectory.Open(GetRandomPath()))) |> exceptionWrapper
 
 [<Fact>]
 let BooleanQueryCreationTests() = 
@@ -120,14 +93,12 @@ let SimpleSpatialTests() =
     let strategy = new RecursivePrefixTreeStrategy(grid, "myGeoField")
     let doc = new Document()
     doc.Add(new IntField("id", 1, Field.Store.YES))
-    let pt = ctx.MakePoint (10.0, 10.0)
+    let pt = ctx.MakePoint(10.0, 10.0)
     doc.Add(new StoredField(strategy.GetFieldName(), pt.getX().ToString() + " " + pt.getY().ToString()))
-
 
 [<EntryPoint>]
 let main argv = 
-    [| CodecsShouldLoadProperly; SimpleIndexingTest; BooleanQueryCreationTests; SimplePhysicalIndexingTest; 
-       SimpleFlexPhysicalIndexingTest; RangeQueryCreationTests; SimpleSpatialTests |] 
+    [| CodecsShouldLoadProperly; IndexingTests; BooleanQueryCreationTests; RangeQueryCreationTests; SimpleSpatialTests |] 
     |> Array.iter (fun meth -> exceptionWrapper meth)
     printfn "Done"
     if hasErrors then 1
